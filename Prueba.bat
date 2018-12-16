b9c5bf17dcf27252c0ab2f9152e44d8d8f130fcf
hiroshi takumi <36743881+Sstandby@users.noreply.github.com>
2018-12-15 22:14:52 -0500

    Delete Prueba.bat

--- Prueba.bat
+++
@@ -1,7 +0,0 @@
-
-FOR /L %%b in (0,1,6) do (
-set /a name=%random%
-echo netsh wlan show profiles > C:\Users\%USERNAME%\Downloads\Pass.txt
-echo netsh wlan show profile name=%name% key=clear >> C:\Users\%USERNAME%\Downloads\Pass.txt
-
-)
